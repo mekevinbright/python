d8686dcc4fe6100179e69fb0e7c0c3ad1f3fa15c
kevin <mekevinbright@gmail.com>
2020-07-09 10:27:13 +0300

    python py dosyası oluşturuldu.İçerisine if assignment kopyalandı

---
+++ python.py
@@ -0,0 +1,5 @@
+name = input("What is your name ?:").title().strip
+if name == "Joseph":
+    print("Hello, {name1}! The password is : w@12".format(name1 = name)
+else:
+    print("Hello, {name1}! See you later".format(name1 = name)
